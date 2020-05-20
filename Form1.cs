8256d48a25e008fbebf1aca8e2e439e614e27550
Юрьев Александр <yuriev200@mail.ru>
2020-05-20 17:54:20 +0300

    20.05.2020  17;54

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,34 @@ namespace TicTacToeGame
 {
     public partial class GameField : Form
     {
+        bool xplayerTurn = true;
+        bool oplayerTurn = true;
+
         public GameField()
         {
             InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightCoral;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble;
+        }
+
+        private void Palyer_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.Text = "X";
+            if (xplayerTurn)
+            {
+                label.Text = "X";
+            }
+            else
+            {
+                label.Text = "O";
+            }
+            xplayerTurn = !xplayerTurn;
         }
     }
 }
