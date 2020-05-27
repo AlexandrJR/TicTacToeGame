70a951ed2a70ce1972f890b28d3fa51a5a0669b0
Юрьев Александр <yuriev200@mail.ru>
2020-05-27 17:47:02 +0300

    27.05.20     17:46

--- Form1.cs
+++ Form1.cs
@@ -35,11 +35,13 @@ namespace TicTacToeGame
             {
                 labelName = "label" + i;
                 Grid.Controls[labelName].Text = "";
+                Grid.Controls[labelName].BackColor = Color.LightCoral;
             }
         }
 
         private void RestartGame()
         {
+            InitializeGrid();
             InitializeCells();
             turnValue = 0;
         }
@@ -82,8 +84,60 @@ namespace TicTacToeGame
             }
         }
 
+        private void WinnerCellsChangeColor()
+        {
+            if (label1.Text == label2.Text && label1.Text == label3.Text && label1.Text != "")
+            {
+                ChangeCellsColor(label1, label2, label3, Color.Goldenrod);
+            }
+
+            else if (label4.Text == label5.Text && label4.Text == label6.Text && label4.Text != "")
+            {
+                ChangeCellsColor(label4, label5, label6, Color.Goldenrod);
+            }
+
+            else if (label7.Text == label8.Text && label7.Text == label9.Text && label7.Text != "")
+            {
+                ChangeCellsColor(label7, label8, label9, Color.Goldenrod);
+            }
+
+            else if (label1.Text == label4.Text && label1.Text == label7.Text && label1.Text != "")
+            {
+                ChangeCellsColor(label1, label4, label7, Color.Goldenrod);
+            }
+
+            else if (label2.Text == label5.Text && label2.Text == label8.Text && label2.Text != "")
+            {
+                ChangeCellsColor(label2, label5, label8, Color.Goldenrod);
+            }
+
+            else if (label3.Text == label6.Text && label3.Text == label9.Text && label3.Text != "")
+            {
+                ChangeCellsColor(label3, label6, label9, Color.Goldenrod);
+            }
+
+            else if (label1.Text == label5.Text && label1.Text == label9.Text && label1.Text != "")
+            {
+                ChangeCellsColor(label1, label5, label9, Color.Goldenrod);
+            }
+
+            else if (label3.Text == label5.Text && label3.Text == label7.Text && label3.Text != "")
+            {
+                ChangeCellsColor(label3, label5, label7, Color.Goldenrod);
+            }
+
+        }
+
+        private void ChangeCellsColor(Label firstlabel, Label secondLable, Label thirdLabel, Color color)
+        {
+            firstlabel.BackColor = color;
+            secondLable.BackColor = color;
+            thirdLabel.BackColor = color;
+        }
+
         private void GameOver()
         {
+            WinnerCellsChangeColor();
             if (xPlayerTurn)
             {
                 MessageBox.Show("X player won!");
